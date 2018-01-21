b33d01c1f00421a5b0e9802e521e142cd4e03298
Marcin Leśnierowski <m.lesnierowski@gmail.com>
2018-01-21 11:52:14 +0100

    testy + dokumentacja

--- src/main/java/pl/put/poznan/scenariomanager/data/model/scenario/visitor/impl/NotActorVisitor.java
+++ src/main/java/pl/put/poznan/scenariomanager/data/model/scenario/visitor/impl/NotActorVisitor.java
@@ -7,22 +7,38 @@ import pl.put.poznan.scenariomanager.data.model.scenario.visitor.ScenarioVisitor
 import java.util.ArrayList;
 
 /**
- * Created by Marcin on 11.01.2018.
+ * ScenarioVisitor, that search for Steps which don't begin with an actor.
  */
 public class NotActorVisitor implements ScenarioVisitor {
 
+
+    /**
+     * Array to collect matching Scenario steps
+     */
     private ArrayList<ScenarioStep> noActorSteps;
 
+    /**
+     * Create new @ArrayList of ScenarioSteps on initialization
+     */
     public NotActorVisitor() {
         noActorSteps = new ArrayList<>();
     }
 
+    /**
+     * This method is invoked on each step in the scenario step hierarchy
+     *
+     * @param step Scenario step which is currently being visited
+     */
     @Override
     public void visit(ScenarioStep step) {
         if(step.getClass() != ActorScenarioStep.class)
             noActorSteps.add(step);
     }
 
+    /**
+     * Get @ArrayList of matching steps
+     * @return
+     */
     public ArrayList<ScenarioStep> getNoActorSteps(){
         return  noActorSteps;
     }
