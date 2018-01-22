dae164180603ebd491ba1fb7560ff0262bcdeb08
Fehu4 <areoflo@gazeta.pl>
2018-01-22 16:31:30 +0100

    Add LevelScenarioVisitor and tests

--- src/main/java/pl/put/poznan/scenariomanager/app/ScenarioManagerApplication.java
+++ src/main/java/pl/put/poznan/scenariomanager/app/ScenarioManagerApplication.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pl.put.poznan.scenariomanager.data.model.scenario.visitor.impl.IndexScenarioVisitor;
 
 
 @SpringBootApplication(scanBasePackages = {
